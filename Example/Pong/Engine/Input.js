28eae5e20f396112c9c372b71efcab6a2d8b8e68
Filipe Dias <filipediascontato@gmail.com>
2019-05-25 23:31:42 -0300

    Implementação dos metodos estaticos do Input

--- Example/Pong/Engine/Input.js
+++ Example/Pong/Engine/Input.js
@@ -1,3 +1,24 @@
 class Input{
+
+    static isDown(keyCode){
+        return this.pressed[keyCode];
+    }
+
     
+    static onKeyDown(event){
+        this.pressed[event.keyCode] = true;
+    }
+
+
+    static onKeyUp(event){
+        delete this.pressed[event.keyCode];
+    }
 }
+
+
+// Static property
+this.pressed;
+this.keyLeft = 37;
+this.keyUp = 38;
+this.keyRight = 39;
+this.keyDown = 40;
