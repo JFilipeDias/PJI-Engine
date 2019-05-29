79b0a79a09fb5343659bc5fdd016ba1331eaa000
Filipe Dias <filipediascontato@gmail.com>
2019-05-29 09:53:21 -0300

    Input por teclado foi implementado

--- Example/Pong/Engine/Input.js
+++ Example/Pong/Engine/Input.js
@@ -1,30 +1,16 @@
-let _pressed;
-
 class Input {
-    static get pressed() {
-        return _pressed;
-    }
-
-
-    static set pressed(pressed){
-        _pressed = pressed;
-    }
-
 
     static getKeyDown(keyCode) {
-        console.log(this.pressed + keyCode);
-        return this.pressed == keyCode;
+        return Input.pressed == keyCode;
     }
 
     
     static onKeyDown(event) {
-        this.pressed = event.code;
-        
-        //console.log("Apertou " + this.pressed);
+        Input.pressed = event.code;
     }
 
     
     static onKeyUp(){
-        this.pressed = '';
+        Input.pressed = '';
     }
 }
