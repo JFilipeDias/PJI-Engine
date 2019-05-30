ba0a8b54d2a726cc93eace7e16800d99a18c8e29
Filipe Dias <filipediascontato@gmail.com>
2019-05-30 13:17:25 -0300

    Desacoplando a colisão

--- Example/Pong/Engine/World.js
+++ Example/Pong/Engine/World.js
@@ -69,43 +69,9 @@ class World {
             // Pears checking
             if(Hierarchy.entityList.length > 1) {
                 for(var count1 = count0 + 1; count1 < Hierarchy.entityList.length; count1++)
-                    this.checkColision(Hierarchy.entityList[count0], Hierarchy.entityList[count1]);
+                    Collision.checkColision(Hierarchy.entityList[count0], Hierarchy.entityList[count1]);
                     
             }
-        }
-    }
-
-
-    checkColision(entity1, entity2) {
-        var minimumX1 = entity1.positionX - entity1.width/2;
-        var minimumY1 = entity1.positionY - entity1.height/2;
-        var maximumX1 = entity1.positionX + entity1.width/2;
-        var maximumY1 = entity1.positionY + entity1.height/2;
-
-        var minimumX2 = entity2.positionX - entity2.width/2;
-        var minimumY2 = entity2.positionY - entity2.height/2;
-        var maximumX2 = entity2.positionX + entity2.width/2;
-        var maximumY2 = entity2.positionY + entity2.height/2;
-
-
-        if(maximumX1 > minimumX2 &&
-            minimumX1 < maximumX2 &&
-            maximumY1 > minimumY2 &&
-            minimumY1 < maximumY2) {
-            
-            if(!entity1.isColliding) {
-                entity1.isColliding = true;
-                entity1.onCollisionEnter(entity2);
-            }
-
-            if(!entity2.isColliding) {
-                entity2.isColliding = true;
-                entity2.onCollisionEnter(entity1);
-            }
-        }
-        else {
-            entity1.isColliding = false;
-            entity2.isColliding = false;
         }
     }
 }
